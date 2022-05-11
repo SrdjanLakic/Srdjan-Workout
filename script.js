0c709e6d8794338ee8ca8bc2404ced67ebe6f679
SrdjanLakic <srdjan949@gmail.com>
2022-05-11 18:43:22 -0400

    Implemented my coordinates in geolocation

--- script.js
+++ script.js
@@ -13,17 +13,18 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
-      console.log(position);
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-      const map = L.map('map').setView([51.505, -0.09], 13);
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
-      L.marker([51.5, -0.09])
+      L.marker(coords)
         .addTo(map)
         .bindPopup('A pretty CSS3 popup.<br> Easily cusomizable.')
         .openPopup();
