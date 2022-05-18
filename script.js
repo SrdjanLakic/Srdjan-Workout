6dbade55bd52454547121a843ce571d586bd6fb3
SrdjanLakic <srdjan949@gmail.com>
2022-05-18 17:04:05 -0400

    class Cycling implemented

--- script.js
+++ script.js
@@ -32,6 +32,18 @@ class Running extends Workout {
   }
 }
 
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    this.calcSpeed = this.distance / (this.duration / 60);
+    return this.calcSpeed;
+  }
+}
+
 class App {
   #mapEvent;
   #map;
