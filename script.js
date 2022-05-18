e65e7d9660be21659410055dd407dd212e4f6408
SrdjanLakic <srdjan949@gmail.com>
2022-05-18 17:10:50 -0400

    class Workout ID fixed

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 class Workout {
   date = new Date();
-  id = (new Date() + '').slice(-10);
+  id = (Date.now() + '').slice(-10);
   constructor(coords, distance, duration) {
     this.coords = coords;
     this.distance = distance;
@@ -44,6 +44,10 @@ class Cycling extends Workout {
   }
 }
 
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const cycling1 = new Cycling([39, -12], 27, 95, 523);
+
+console.log(run1, cycling1);
 class App {
   #mapEvent;
   #map;
